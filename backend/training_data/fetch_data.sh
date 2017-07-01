a2fe160bd9284ae2fd41e118dd517899dde8b231
Bela Stoyan <bela@nichtbela.de>
2017-07-01 13:55:26 +0200

    New lib

--- backend/training_data/fetch_data.sh
+++ backend/training_data/fetch_data.sh
@@ -1,6 +1,7 @@
+set -ue
+
 mkdir partys
 mkdir mdbs
-source twitter_keys.sh
 python3 get_abgeordnete.py
 python3 load_bios.py
 python3 get_accounts_from_twde_list.py
--- backend/training_data/get_accounts_from_twde_list.py
+++ backend/training_data/get_accounts_from_twde_list.py
@@ -1,31 +1,28 @@
 import os
 import json
-import twitter
+from twitter import *
 
 from load_bios import create
 
-api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
-                  consumer_secret=os.environ['CONSUMER_SECRET'],
-                  access_token_key=os.environ['ACCESS_TOKEN_KEY'],
-                  access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
+api = Twitter(auth=OAuth(os.environ['ACCESS_TOKEN_KEY'], os.environ['ACCESS_TOKEN_SECRET'], os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET']))
 
-statuses = api.GetListMembers(list_id=67426883)
+statuses = api.lists.members(list_id=67426883)
 
 with open("mdbs.json", 'r') as f:
     mdbs = json.load(f)
 
 parties = ('CDU', 'CSU', 'Die Linke', 'SPD', 'Bündnis 90\\Die Grünen', 'fraktionslos')
 
-for s in statuses:
+for s in statuses["users"]:
     party = ""
-    print(s.screen_name, s.name)
+    print(s['screen_name'], s['name'])
     for key in mdbs.keys():
-        if key in s.name or s.name in key:
+        if key in s['screen_name'] or s['name'] in key:
             party = mdbs[key]
     if not party:
-        print("skipped " + s.name)
+        print("skipped " + s['name'])
         continue
         # i = int(input("Party: "))
         # party = parties[i]
     print(party)
-    create(party, "https://twitter.com/"+s.screen_name)
+    create(party, "https://twitter.com/"+s['screen_name'])
