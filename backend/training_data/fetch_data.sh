cb0ecd89006f85c8f97af5dc330d0f48c1c407c0
StoneInc <levyehrstein@googlemail.com>
2017-07-01 12:35:53 +0000

    fetch_data now prints the amount of received tweets

--- backend/training_data/fetch_data.sh
+++ backend/training_data/fetch_data.sh
@@ -5,4 +5,6 @@ python3 get_abgeordnete.py
 python3 load_bios.py
 python3 get_accounts_from_twde_list.py
 python3 get_afd_kandidaten.py
-./golang_retrive_tweets
+./golang_retrive_tweets 
+amnt=$(ls -R -asl | grep .TXT | wc -l)
+echo Read $amnt tweets!
