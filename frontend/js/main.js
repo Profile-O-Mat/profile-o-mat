0542cb6000baa98131a3dc87bd8c1c9c9f1700f5
SparxDev <sparxdev@outlook.de>
2017-07-01 23:59:53 +0200

    Updated files

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,30 @@
+// Datum richtig formatieren
+function formatDate(d) {
+  return d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes()
+}
+
+// Main function
+$(function() {
+  t = document.getElementById('timeline')
+
+  // Web Socket
+  var ws = new WebSocket("ws://192.168.2.164:8000");
+
+  // Connection bestätigung
+  ws.onopen = function(){
+    console.log("Socket has been opened!");
+  };
+
+  // Nachrichten anzeigen
+  ws.onmessage = function(message) {
+    message = JSON.parse(message.data);
+    console.log(message);
+    var tweet = '<div class="tweet"><img src="' + message["profile_img"] + '" class="tweet-avatar"><div class="tweet-political-real ' + message["real_party"] + '">' + message["real_party"] + '</div><div class="tweet-political-bot ' + message["guessed_party"] + '">' + message["guessed_party"] + '</div><div class="tweet-content"><h1>' + message["name"] + '</h1><h2>@' + message["handle"] + '</h2><p>' + message["text"] + '</p><p class="timestamp">' + formatDate(new Date(message["date"])) + '</p></div></div>';
+    $(tweet).prependTo($(".timeline")).hide().show("drop", 500);
+  };
+});
+
+// smooth scroll
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
